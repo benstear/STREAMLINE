c4d467f5e9aa4f31e177a17f0543b2f45f165cdd
ben stear <bjs385@drexel.edu>
2022-12-02 15:59:33 -0500

    Update FeatureImportanceMain.py

--- streamline/FeatureImportanceMain.py
+++ streamline/FeatureImportanceMain.py
@@ -205,9 +205,9 @@ def submitSlurmClusterJob(command_text,experiment_path,reserved_memory,maximum_m
     sh_file.write('#SBATCH -o ' + experiment_path+'/logs/P3_'+job_ref+'.o\n')         ## Send standard output to file path
     sh_file.write('#SBATCH -e ' + experiment_path+'/logs/P3_'+job_ref+'.e\n')         ## Send standard error to file path
     
-    sh_file.write('load module python3\n')                ## Load the python interpreter
+    sh_file.write('module load python3\n')                ## Load the python interpreter
     ###  SBATCH --mem-per-cpu=100M     ## Real memory(MB) per CPU required by the job
-    sh_file.write('pip3 install --user skrebate==0.7 xgboost lightgbm catboost gplearn scikit-eLCS scikit-XCS scikit-ExSTraCS optuna==2.0.0 plotly kaleido fpdf scipy')
+    sh_file.write('pip3 install --user skrebate==0.7 xgboost lightgbm catboost gplearn scikit-eLCS scikit-XCS scikit-ExSTraCS optuna==2.0.0 plotly kaleido fpdf scipy\n')
     
     this_file_path = os.path.dirname(os.path.realpath(__file__))
     #sh_file.write(+'\n')
